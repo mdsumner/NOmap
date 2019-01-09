4368fb4c4ef6a5bb9ecbc645fe3590c1810e975e
Dale Maschette <dale.maschette@aad.gov.au>
2019-01-09 16:24:44 +1100

    Pretty sure I actually fixed the test conflicts this time :whale::fish::blowfish:

--- tests/testthat/test-automap.R
+++ tests/testthat/test-automap.R
@@ -5,7 +5,7 @@ disp_auto_map <- function() SOauto_map(c(100:110), c(-70:-60))
 test_that("auto map returns data", {
   expect_s3_class(x <- disp_auto_map(), "SOmap")
   vdiffr::expect_doppelganger("Soauto_map basic", disp_auto_map)
-  expect_that(sort(names(x)), equals(c("bathy", "coastline", "crs", "data", "graticule", "target")))
+  expect_that(sort(names(x)), equals(c("bathy", "coastline", "crs", "data", "graticule","oldpar", "target")))
 })
 
 disp_resblocks <- function() SOauto_map(SOmap_data$CCAMLR_research_blocks[c(1, 4, 5), ])
