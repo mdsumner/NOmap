a5bfbeb0fa3561ff39567c7e4d14aaf85fabeded
Ben Raymond <ben.raymond@aad.gov.au>
2019-02-27 19:22:20 +1100

    fix missing @importFrom

--- R/SOmap-package.R
+++ R/SOmap-package.R
@@ -5,7 +5,7 @@
 #' @name SOmap-package
 #' @docType package
 #' @importFrom assertthat assert_that is.flag
-#' @importFrom ggplot2 aes_string coord_sf element_blank fortify ggplot geom_path geom_polygon geom_raster geom_sf geom_text labs scale_fill_gradientn theme
+#' @importFrom ggplot2 aes_string coord_sf element_blank fortify ggplot geom_path geom_polygon geom_raster geom_sf geom_sf_text geom_text labs scale_fill_gradientn theme
 #' @importFrom graphics contour lines par plot plot.new plot.window points text
 #' @importFrom grDevices heat.colors
 #' @importFrom methods as
