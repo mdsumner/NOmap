172669b0a8e8de0d0310a08aee135a526407fd84
Dale Maschette <dale.maschette@aad.gov.au>
2019-02-27 22:32:28 +1100

    Fixing the missing commas because I'm an idiot :whale::fish::blowfish:

--- R/SOplot.R
+++ R/SOplot.R
@@ -31,13 +31,13 @@
 #'  y<-c(-50, -75, -45, -60)
 #'  map<-SOauto_map(x,y, input_lines = FALSE)
 #'  map
-#'  SOplot(lon=y, lat=x,crs = map$crs,pch=19,col=1)
+#'  SOplot(lon=y, lat=x,crs = map$crs,pch=19,col=6)
 #' }
 #' @export
 #'
 
 
-SOplot<-function(lon, lat,prj, crs,add=TRUE...){
+SOplot<-function(lon, lat,prj, crs,add=TRUE,...){
   SObj<-SOproj(lon,lat,prj, crs)
-  plot(SObj,add=add...)
+  plot(SObj,add=add,...)
 }
