3386dc76bafec9c3881782ebfddb44b94cb83449
Dale Maschette <dale.maschette@aad.gov.au>
2019-02-27 22:04:54 +1100

    First crack at SOproj and SOplot :whale::fish::blowfish:

---
+++ R/SOplot.R
@@ -0,0 +1,43 @@
+
+#' Southern Ocean plot
+#'
+#' @description
+#' Reproject and add point layers to either SOmap or SOauto_map layers.
+#'
+#' @param lon
+#' longitude object
+#'
+#' @param lat
+#' lattitude object
+#'
+#' @param prj
+#' starting projection (default = longlat)
+#'
+#' @param crs
+#' target projection (default = stereo)
+#'
+#' @param add
+#' add layer to plot (default = TRUE)
+#'
+#' @param ...
+#' other plot options
+#'
+#' @return
+#' Produces at the very base a round bathymetry map of the southern hemisphere.
+#'
+#' @examples
+#' \dontrun{
+#'  x<-c(-70, -60,-50, -90)
+#'  y<-c(-50, -75, -45, -60)
+#'  map<-SOauto_map(x,y, input_lines = FALSE)
+#'  map
+#'  SOplot(lon=y, lat=x,crs = map$crs,pch=19,col=1)
+#' }
+#' @export
+#'
+
+
+SOplot<-function(lon, lat,prj, crs,add=TRUE...){
+  SObj<-SOproj(lon,lat,prj, crs)
+  plot(SObj,add=add...)
+}
---
+++ R/SOproj.R
@@ -0,0 +1,47 @@
+
+#' Southern projection
+#'
+#' @description
+#' Function for reprojecting data.
+#'
+#' @param lon
+#' longitude object.
+#'
+#' @param lat
+#' lattitude object.
+#'
+#' @param prj
+#' starting projection (default = longlat)
+#'
+#' @param crs
+#' target projection (default = stereo)
+#'
+#' @return
+#' Produces at the very base a round bathymetry map of the southern hemisphere.
+#'
+#' @examples
+#' \dontrun{
+#'  x<-c(-70, -60,-50, -90)
+#'  y<-c(-50, -75, -45, -60)
+#'  pnts<-SOproj(lon = y, lat = x)
+#'  SOmap2(CCAMLR=T)
+#'  plot(pnts, pch=19, col=3, add=TRUE)
+#' }
+#' @export
+#'
+
+SOproj<-function(lon, lat,prj, crs){
+  if(missing(lon) || missing(lat)){
+    stop("lon and lat must be provided")}
+  if(missing(prj) || is.null(prj) || !nzchar(prj)){
+    message("No projection provided, assuming longlat")
+    prj<-"+proj=longlat +datum=WGS84"}
+
+  if(missing(crs)){message("No CRS provided assuming SOmap default")}
+  df<-data.frame(lon=lon,lat=lat)
+  coordinates(df)<-c("lon","lat")
+
+  projection(df)<-prj
+  if(missing(crs)){crs<-"+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"}
+  dft<-spTransform(df,CRS(crs))
+}
