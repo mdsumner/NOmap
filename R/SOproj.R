52b2db6e9a9a541b6bc5c7f52ebb8ed69962640f
Michael Sumner <mdsumner@gmail.com>
2019-03-14 13:19:17 +1100

    reproj methods

--- R/SOproj.R
+++ R/SOproj.R
@@ -32,7 +32,6 @@
 #' }
 #' @export
 #'
-
 SOproj <- function(lon, lat, prj, crs, data){
   if (missing(lon) || missing(lat)) {
       stop("lon and lat must be provided")
@@ -51,5 +50,68 @@ SOproj <- function(lon, lat, prj, crs, data){
   if(missing(crs)) {
       crs <- "+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
   }
-  dft <- sp::spTransform(df, sp::CRS(crs))
+  sp::spTransform(df, sp::CRS(crs))
+}
+
+#' @importFrom raster projection
+projection.SOmap <- function(x, asText = TRUE) {
+ raster::projection(x$projection, asText = asText)
+}
+#' Reproject SOmap
+#'
+#' Reproject a SOmap object by specifying a 'target' projection string (PROJ4)
+#'
+#' See [reproj::reproj()] for details.
+#'
+#' @section Warning:
+#' So many ...
+#' @seealso reproj::reproj
+#' @export reproj
+#' @inheritParams reproj::reproj
+#' @export
+#' @aliases reproj.SOauto_map
+#' @importFrom reproj reproj
+#' @importFrom raster projectRaster raster
+#' @examples
+#' set.seed(27)
+#' amap <- SOauto_map()
+#' reproj(amap, "+proj=moll")
+#' reproj(amap, "+proj=laea +lat_0=-55 +lon_0=154 +datum=WGS84")
+#'
+#' bmap <- SOmap(Trim = -35)
+#'
+#' reproj(bmap, "+proj=stere +lat_0=-90 +lon_0=147 +lat_ts=-71 +datum=WGS84")
+#' ## these aren't exactly ideal
+#' reproj(bmap, "+proj=ortho +lat_0=-70")
+#' reproj(bmap, "+proj=laea +lat_0=-55 +lon_0=154 +datum=WGS84")
+reproj.SOmap <- function(x, target, ..., source = NULL) {
+  if (missing(target)) stop("'target' projection string required")
+  if (!is.null(source)) warning("source ignored, should be NULL for SOmap objects")
+  rast <- try(raster::projectRaster(x$bathy$plotargs$x, crs = target), silent = TRUE)
+  if (inherits(rast, "try-error")) {
+    stop("unable to reproject raster sensibly")
+  }
+  x$bathy$plotargs$x <- rast
+  x$target <- raster::raster(x$bathy$plotargs$x)
+  ## box?
+  x$coastline$plotargs$x <- sf::st_transform(x$coastline$plotargs$x, target)
+  x$projection <- target
+  x
+}
+#' @export
+#' @name reproj.SOmap
+reproj.SOauto_map <- function(x, target, ..., source = NULL) {
+  if (missing(target)) stop("'target' projection string required")
+  if (!is.null(source)) warning("source ignored, should be NULL for SOmap objects")
+  x$bathy <- raster::projectRaster(x$bathy, crs = target)
+  x$target <- raster::raster(x$bathy)
+  ## box?
+  if (!is.null(x$coastline$data)) x$coastline$data <- sp::spTransform(x$coastline$data, target)
+  if (!is.null(x$graticule)) x$graticule <- sf::st_transform(x$graticule, target)
+
+  if (!is.null(x$lines_data)) x$lines_data <- reproj(x$lines_data, target, source = x$projection)
+  if (!is.null(x$points_data)) x$points_data <- reproj(x$points_data, target, source = x$projection)
+
+  x$projection <- target
+  x
 }
