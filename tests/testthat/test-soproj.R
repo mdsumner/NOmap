2e1ae871ebeac81e02035808ea65ad23dbe42b12
Michael Sumner <mdsumner@gmail.com>
2019-03-20 23:30:09 +1100

    SOproj tests (WIP)

---
+++ tests/testthat/test-soproj.R
@@ -0,0 +1,7 @@
+context("test-soproj")
+
+SOcrs("+proj=laea +datum=WGS84")
+test_that("multiplication works", {
+  expect_error(SOproj(cbind(147, -42), "+proj=laea +datum=WGS84"), "y is character, did you mean 'target = '")
+#  expect_error(SOproj(147), "x and y must be provided unless")
+})
